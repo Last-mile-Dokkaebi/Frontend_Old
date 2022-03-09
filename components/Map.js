662ffa4896f1dc88ce4f7626a34df336ada404d6
LWC421 <lwc421@gmail.com>
2022-03-09 21:10:31 +0900

    마커 오버레이 추가

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { scooterApi } from "../utils/api";
+import { PropTypes } from "prop-types";
 
 const Container = styled.div`
   width: 100%;
@@ -13,15 +14,47 @@ const Map = () => {
   const markers = [];
 
   // map, x(lon), y(lat)을 인자로 받아 해당하는 마커를 지도에 표시합니다.
-  const addMarker = useCallback((map, x, y) => {
+  const addMarker = useCallback((map, x, y, soc) => {
     const coords = new kakao.maps.LatLng(y, x);
     const marker = new kakao.maps.Marker({
       position: coords,
     });
 
+    const content = `<div 
+    style="transform:translate(0, -3rem);width:8rem;height:1.4rem;text-align:center;background-color:white;">
+    배터리 상태 : ${soc}%
+    </div>`;
+    const overlay = new kakao.maps.CustomOverlay({
+      position: coords,
+      content: content,
+    });
+
+    kakao.maps.event.addListener(marker, "mouseover", () => {
+      overlay.setMap(map);
+    });
+    kakao.maps.event.addListener(marker, "mouseout", () => {
+      overlay.setMap(null);
+    });
+
+    kakao.maps.event.addListener(marker, "click", () => {
+      alert(soc);
+    });
+
+    // const content = `<div>${soc}</div>`;
+    // const infoWindow = new kakao.maps.InfoWindow({
+    //   content: content,
+    // });
+
+    // kakao.maps.event.addListener(marker, "mouseover", () => {
+    //   infoWindow.open(map, marker);
+    // });
+    // kakao.maps.event.addListener(marker, "mouseout", () => {
+    //   infoWindow.close();
+    // });
+
     marker.setMap(map);
     markers.push(marker);
-  });
+  }, []);
 
   useEffect(async () => {
     const res = await scooterApi();
@@ -32,16 +65,18 @@ const Map = () => {
       console.log(scooters);
       const lon = [];
       const lat = [];
+      const possibleScooters = [];
       scooters.forEach((scooter) => {
-        lon.push(scooter.lon);
-        lat.push(scooter.lat);
+        if (scooter.status === "POSSIBLE") {
+          possibleScooters.push(scooter);
+          lon.push(scooter.lon);
+          lat.push(scooter.lat);
+        }
       });
-      const x = lon.reduce((prev, current) => prev + current) / scooters.length;
-      const y = lat.reduce((prev, current) => prev + current) / scooters.length;
-
-      //const x = scooters.reduce(
-      //     (prev, current) => parseFloat(prev.lon) + parseFloat(current.lon)
-      //   ) / scooters.length;
+      const x =
+        lon.reduce((prev, current) => prev + current) / possibleScooters.length;
+      const y =
+        lat.reduce((prev, current) => prev + current) / possibleScooters.length;
 
       const coords = new kakao.maps.LatLng(y, x);
       const options = {
@@ -59,9 +94,9 @@ const Map = () => {
       map.relayout();
       map.setCenter(coords);
 
-      for (let i = 0; i < scooters.length; i++) {
-        addMarker(map, lon[i], lat[i]);
-      }
+      possibleScooters.forEach((scooter) => {
+        addMarker(map, scooter.lon, scooter.lat, scooter.soc);
+      });
 
       clusterer.addMarkers(markers);
       const mapTypeControl = new kakao.maps.MapTypeControl();
@@ -77,6 +112,18 @@ const Map = () => {
       <Container ref={kakaoMap} style={{ widht: "100%", height: "100%" }} />
     </Container>
   );
+};
+
+const MouseOver = ({ soc }) => {
+  const Info = styled.div`
+    width: 5rem;
+    height: 1rem;
+  `;
+  return <Info>{soc}</Info>;
+};
+
+MouseOver.propTypes = {
+  soc: PropTypes.number.isRequired,
 };
 
 export default Map;
